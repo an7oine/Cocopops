b4b0826fb0f241812e40f9f082d1a8ca2236e5cd
Antti Hautaniemi <an7oine@me.com>
2015-04-12 19:36:52 +0300

    Pointer arrays

---
+++ Foundation/NSArray+PointerArray.h
@@ -0,0 +1,21 @@
+//
+//  2015 Magna cum laude. PD
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (PointerArray)
+
+// 'malloc' a array, fill it with self's objects (as __bridge void *), then sort the pointers into ascending order;
+// return by reference
+- (void)getSortedPointers:(void ***)pointers count:(size_t *)count;
+
+// 'malloc' a new array, fill it with the given existing pointers + self's objects, then sort everything again;
+// return by reference
+- (void)combineWithSortedPointers:(void ***)pointers count:(size_t *)count;
+
+// check whether a given object is pointed to within the given (__bridge void *) pointers
++ (BOOL)sortedPointers:(void **)pointers count:(size_t)count containObject:(id)obj;
+
+@end
---
+++ Foundation/NSArray+PointerArray.m
@@ -0,0 +1,50 @@
+//
+//  2015 Magna cum laude. PD
+//
+
+#import "NSArray+PointerArray.h"
+
+@implementation NSArray (PointerArray)
+
+- (void)getSortedPointers:(void ***)pointers count:(size_t *)count
+{
+	*count = self.count;
+	*pointers = malloc(sizeof (void *) * *count);
+	size_t pointerIndex=0;
+	for (id obj in self)
+		(*pointers)[pointerIndex++] = (__bridge void *)obj;
+	qsort_b(*pointers, *count, sizeof (void *), ^int(const void *a, const void *b)
+	{
+		return (int)(*(const void **)a - *(const void **)b);
+	});
+}
+
+- (void)combineWithSortedPointers:(void ***)pointers count:(size_t *)count
+{
+	void **combinedPointers = malloc(sizeof (void *) * (*count + self.count));
+	memcpy(combinedPointers, *pointers, sizeof (void *) * *count);
+	size_t pointerIndex = *count;
+	*pointers = combinedPointers;
+	*count = *count + self.count;
+	for (id obj in self)
+		combinedPointers[pointerIndex++] = (__bridge void *)obj;
+	qsort_b(*pointers, *count, sizeof (void *), ^int(const void *a, const void *b)
+	{
+		return (int)(*(const void **)a - *(const void **)b);
+	});
+}
+
++ (BOOL)sortedPointers:(void **)pointers count:(size_t)count containObject:(id)obj
+{
+	size_t i=0;
+	while (i < count)
+		if (pointers[i] == (__bridge void *)obj)
+			return YES;
+		else if (pointers[i] > (__bridge void *)obj)
+			return NO;
+		else
+			i++;
+	return NO;
+}
+
+@end
