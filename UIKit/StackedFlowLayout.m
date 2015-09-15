1986fd3e42dba15a40ab65f5d5e3b81a9225130d
Antti Hautaniemi <antti@mbp.local>
2015-09-15 21:54:19 +0300

    StackedFlowLayout: fix for iOS9 runtime warnings

--- UIKit/StackedFlowLayout.m
+++ UIKit/StackedFlowLayout.m
@@ -4,11 +4,13 @@
 
 #import "StackedFlowLayout.h"
 
+#import "NSArray+DeepCopy.h"
+
 @implementation StackedFlowLayout
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForItemAtIndexPath:indexPath];
+    UICollectionViewLayoutAttributes *attributes = [super layoutAttributesForItemAtIndexPath:indexPath].copy;
 	attributes.zIndex = self.collectionViewContentSize.width - attributes.center.x + attributes.center.y;
 	attributes.transform3D = CATransform3DMakeTranslation(0.0f, 0.0f, attributes.zIndex);
     return attributes;
@@ -16,7 +18,7 @@
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
-    NSArray *allAttributes = [super layoutAttributesForElementsInRect:rect];
+    NSArray *allAttributes = [[super layoutAttributesForElementsInRect:rect] deepCopy];
     for (UICollectionViewLayoutAttributes *attributes in allAttributes)
 	{
 		if (attributes.representedElementCategory == UICollectionElementCategoryCell)
