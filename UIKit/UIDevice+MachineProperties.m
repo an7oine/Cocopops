e797fe9695875f7076f527454471296eb0e7c746
Antti Hautaniemi <antti@VMware.local>
2015-11-19 12:00:50 +0200

    UIDevice+MachineProperties: reflected Apple lineup of 2015

--- UIKit/UIDevice+MachineProperties.m
+++ UIKit/UIDevice+MachineProperties.m
@@ -9,7 +9,7 @@
 
 typedef struct
 {
-    enum { kMachineSimulator = 0, kMachinePod = 1, kMachinePhone = 2, kMachinePad = 3 } kind;
+    enum { kMachineSimulator = 0, kMachinePod = 1, kMachinePhone = 2, kMachinePad = 3, kMachineTV } kind;
     short major, minor;
 } machine_t;
 
@@ -25,14 +25,16 @@ typedef struct
 - (machine_t)machine
 {
     NSString *machineString = self.machineString;
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(iPod|iPhone|iPad)([0-9]+),([0-9]+)" options:0 error:NULL];
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(iPod|iPhone|iPad|AppleTV)([0-9]+),([0-9]+)" options:0 error:NULL];
     NSArray *c = [regex matchesInString:machineString options:0 range:NSMakeRange(0, machineString.length)];
     if (c.count >= 3)
         return (machine_t)
     {
-        [c[0] isEqualToString:@"iPod"]? kMachinePod :
-        [c[0] isEqualToString:@"iPhone"]? kMachinePhone :
-        [c[0] isEqualToString:@"iPad"]? kMachinePad : kMachineSimulator,
+        [c[0] isEqualToString:@"iPod"]?		kMachinePod :
+        [c[0] isEqualToString:@"iPhone"]?	kMachinePhone :
+		[c[0] isEqualToString:@"iPad"]?		kMachinePad :
+		[c[0] isEqualToString:@"AppleTV"]?	kMachineTV :
+											kMachineSimulator,
         [c[1] shortValue],
         [c[2] shortValue]
     };
@@ -45,38 +47,52 @@ typedef struct
     machine_t machine = self.machine;
     
     if (machine.kind == kMachineSimulator)
-        // iPhone Simulator: rely on the UI idiom (wrong results on iPhone 6+, iPad Mini)
+        // iPhone Simulator: rely on the UI idiom (wrong results on iPhone 6(S)+, iPad Mini)
         switch (self.userInterfaceIdiom)
         {
             case UIUserInterfaceIdiomPad: return 132.0f;
-            case UIUserInterfaceIdiomPhone: return 163.0f;
-#if __TV_OS_VERSION_MAX_ALLOWED >= 90000
-            case UIUserInterfaceIdiomTV: return 55.0f; // 40 inch display at 1080p
+			case UIUserInterfaceIdiomPhone: return 163.0f;
+#if TARGET_OS_TV
+            case UIUserInterfaceIdiomTV: return 55.0f; // ~40 inch display driven at 1080p
 #endif
             default: return 163.0f;
         }
+	
+	else if (machine.kind == kMachineTV)
+		// Apple TV: single pixels, pixels-per-inch dependent on connected display
+		// TODO: read actual display parameters once such an API exists within tvOS
+		return 55.0f; // ~40 inch display driven at 1080p
     
     else if (machine.kind == kMachinePhone && machine.major == 7 && machine.minor == 1)
-        // iPhone 6 Plus (triple pixels, 401 PixPI)
+        // iPhone 6 Plus: triple pixels, 401 pixels-per-inch
         return 133.667f;
-    
+	
+	else if (machine.kind == kMachinePhone && machine.major == 8 && machine.minor == 2)
+		// iPhone 6S Plus: triple pixels, 401 pixels-per-inch
+		return 133.667f;
+	
     else if (machine.kind != kMachinePad)
-        // iPhone, 3G, 3GS / iPod Touch 1st, 2nd, or 3rd gen (single pixels)
-        // iPhone 4, 4S, 5, 5C, 5S, 6 / iPod Touch 4th or 5th gen (double pixels, 326 PixPI)
+        // iPhone, 3G, 3GS / iPod Touch 1st, 2nd, or 3rd gen: single pixels
+        // iPhone / iPod Touch (all later models): double pixels, 326 pixels-per-inch
         return 163.0f;
     
-    else if (machine.major == 2 && machine.minor >= 5)
-        // iPad Mini 1 (single pixels)
+    else if (machine.major == 2 && machine.minor >= 5 && machine.minor <= 7)
+        // iPad Mini 1: single pixels
         return 163.0f;
     
-    else if (machine.major == 4 && machine.minor >= 4)
-        // iPad Mini 2 or 3 (double pixels, 326 PixPI)
+    else if (machine.major == 4 && machine.minor >= 4 && machine.minor <= 9)
+        // iPad Mini 2 or 3: double pixels, 326 pixels-per-inch
         return 163.0f;
-    
+	
+	else if (machine.major == 5 && machine.minor >= 1 && machine.minor <= 2)
+		// iPad 4: double pixels, 326 pixels-per-inch
+		return 163.0f;
+	
     else
-        // iPad or iPad 2 (single pixels)
-        // iPad, 3rd or 4th gen (double pixels, 264 PixPI)
-        // iPad Air 1 or 2 (double pixels, 264 PixPI)
+        // iPad or iPad 2: single pixels
+        // iPad, 3rd or 4th gen: double pixels, 264 pixels-per-inch
+        // iPad Air 1 or 2: double pixels, 264 pixels-per-inch
+		// iPad Pro: double pixels, 264 pixels-per-inch
         return 132.0f;
 }
 
